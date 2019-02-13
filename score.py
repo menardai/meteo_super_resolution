37c718c309d83d27c48e51ad7de16e58a415f44e
Stephane Menard <stephane@friendstrip.com>
2019-02-13 15:46:19 -0500

    fixed the score function

--- score.py
+++ score.py
@@ -20,31 +20,62 @@ import numpy as np
 from scoreFunction import *
 
 
-label_test= np.load('./data/label_test_set.npy')
+def compute_scores(generated_test, label_test, verbose=False):
+    """
+    Compute the score for each image.
 
-#lowres_test= np.load('./data/input_test_set.npy')
-#print('lowres:', lowres_test.shape)
-#lowres_scores = getScore(lowres_test[:,5,:,:].reshape(248,1,256,256), label_test)
-#print('lowres score (mean) =', np.mean(lowres_scores))
+    Args:
+        generated_test: list (n,1,256,256) of generated test image
+        label_test: list (n,1,256,256) of the label test image
+        verbose: print score if verbose is True
+    Return:
+        a list of score
+    """
+    generated_scores = []
+    for i, gen_img, label_img in zip(range(len(generated_test)), generated_test, label_test):
+        score = getScore(gen_img.reshape(1,1,256,256), label_img.reshape(1,1,256,256)).item()
 
-generated_test = np.load('./images/image_gen_test.npy')
+        if verbose:
+            print('{} - {:0.4f}'.format(i, score))
 
-print('label : ', label_test.shape)
-print('generated:', generated_test.shape)
-print('------------------------------')
+        generated_scores.append(score)
 
-generated_scores = getScore(generated_test, label_test)
+    return generated_scores
 
-date_times = np.load('./data/date_test_set.npy')
 
-with open('./images/image_gen_test_score.csv', 'w') as f:
-    for i in range(len(date_times)):
-        date_time = date_times[i].decode('UTF-8')
-        score = generated_scores[i]
+def save_scores(generated_scores, date_times, output_csv_filename, verbose=False):
+    with open(output_csv_filename, 'w') as f:
+        for i in range(len(date_times)):
+            date_time = date_times[i].decode('UTF-8')
+            score = generated_scores[i]
 
-        print('{} --> {:10.4f}'.format(date_time, score))
-        f.write('"{}","{:10.4f}"\n'.format(date_time, score))
+            if verbose:
+                print('{} --> {:0.4f}'.format(date_time, score))
 
-print('generated score (mean) =', np.mean(generated_scores))
+            f.write('"{}","{:0.4f}"\n'.format(date_time, score))
 
-print('\nThe score for each image has been saved in this file:\n./images/image_gen_test_score.csv')
+
+if __name__ == "__main__":
+    date_times = np.load('./data/date_test_set.npy')
+    label_test = np.load('./data/label_test_set.npy')
+
+    generated_test = np.load('./images/image_gen_test.npy')
+
+    print('label : ', label_test.shape)
+    print('generated:', generated_test.shape)
+    print('------------------------------')
+
+    output_csv_filename = './images/image_gen_test_score.csv'
+    generated_scores = compute_scores(generated_test, label_test, verbose=True)
+    save_scores(generated_scores, date_times, output_csv_filename, verbose=True)
+
+    print('generated score (mean) =', np.mean(generated_scores))
+    print('\nThe score for each image has been saved in this file:\n{}'.format(output_csv_filename))
+
+    # ----------------------
+    # compute score for baseline (lowres images against label images)
+    # lowres score (mean) = 10.5384
+    #lowres_test= np.load('./data/input_test_set.npy')
+    #lowres_scores = compute_scores(lowres_test[:,5,:,:].reshape(248,1,256,256), label_test, verbose=True)
+    #print('lowres score (mean) =', np.mean(lowres_scores))
+
